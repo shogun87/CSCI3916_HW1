3b7b465cb03a26b921c14865e890828a084d2121
shogun87 <45832125+shogun87@users.noreply.github.com>
2022-01-31 16:17:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var server = require("http").createServer();
+
+server.on("request", (request, response) => {
+    var body = [];
+    request.on("data", chunk => {
+        body.push(chunk);
+    });
+    request
+        .on("end", () => {
+            let bodyString = body.concat().toString();
+            console.log(bodyString);
+            response.end(bodyString);
+        })
+        .on("error", () => {
+            response.statusCode = 400;
+            response.end();
+        });
+    response.on("error", err => {
+        console.error(err);
+    });
+});
+server.listen(process.env.PORT || 8008, () => {
+    console.log("Server listening at 8008");
+});
+
+//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
